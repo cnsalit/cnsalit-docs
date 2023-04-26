c7d98b30a149c6c9c2d0fa0685c14c7cadf01ca9
quincy <insta0430@gmail.com>
2023-04-26 15:32:27 +0900

    Chore: 변경 한국어로
    html에 메타데이터로 표시되는 언어를 한국어로 변경했습니다.

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  lang: 'ko-KR',
   title: "큰별문학상 개발자 문서",
   description: "큰별문학상 사이트 개발 과정을 소개합니다.",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
+      { text: '안녕하세요!', link: '/' },
       { text: 'Examples', link: '/markdown-examples' }
     ],
 
